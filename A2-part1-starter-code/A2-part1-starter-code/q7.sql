395e139c818eac4787c91947416acfddc2c2d62b
Fakhoury Anthony <fakhou11@teach.cs.toronto.edu>
2022-10-13 22:02:20 -0400

    first try of Q7

--- A2-part1-starter-code/A2-part1-starter-code/q7.sql
+++ A2-part1-starter-code/A2-part1-starter-code/q7.sql
@@ -17,10 +17,55 @@ CREATE TABLE q7(
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
 DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS driverRated CASCADE;
+DROP VIEW IF EXISTS fivestars CASCADE;
+DROP VIEW IF EXISTS fourstars CASCADE;
+DROP VIEW IF EXISTS threestars CASCADE;
+DROP VIEW IF EXISTS twostars CASCADE;
+DROP VIEW IF EXISTS onestars CASCADE;
+DROP VIEW IF EXISTS allstars CASCADE;
 
 -- Define views for your intermediate steps here:
+CREATE VIEW driverRated AS
+SELECT driver_id, rating
+FROM ClockedIn NATURAL JOIN Dispatch NATURAL JOIN DriverRating;
 
+CREATE VIEW fivestars AS
+SELECT driver_id, sum(rating) as r5
+FROM driverRated
+WHERE rating = 5
+GROUP BY driver_id;
+
+CREATE VIEW fourstars AS
+SELECT driver_id, sum(rating) as r4
+FROM driverRated
+WHERE rating = 4
+GROUP BY driver_id;
+
+CREATE VIEW threestars AS
+SELECT driver_id, sum(rating) as r3
+FROM driverRated
+WHERE rating = 3
+GROUP BY driver_id;
+
+CREATE VIEW twostars AS
+SELECT driver_id, sum(rating) as r2
+FROM driverRated
+WHERE rating = 2
+GROUP BY driver_id;
+
+CREATE VIEW onestars AS
+SELECT driver_id, sum(rating)as r1
+FROM driverRated
+WHERE rating = 1
+GROUP BY driver_id;
+
+CREATE VIEW allstars AS
+SELECT driver_id, r5, r4, r3, r2, r1
+FROM fivestars, fourstars, threestars, twostars, onestars
+GROUP BY driver_id;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q7
+SELECT Driver.driver_id COALESCE(r5, 0), COALESCE(r4, 0), COALESCE(r3, 0), COALESCE(r2, 0), COALESCE(r1, 0)
+FROM allstars RIGHT NATURAL JOIN Driver; 
