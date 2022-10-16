72983ca50e08a5f8977477364b2fe2afbd31af3e
Fakhoury Anthony <fakhou11@teach.cs.toronto.edu>
2022-10-16 19:16:10 -0400

    Q8

--- A2-part1-starter-code/A2-part1-starter-code/q8.sql
+++ A2-part1-starter-code/A2-part1-starter-code/q8.sql
@@ -14,10 +14,21 @@ CREATE TABLE q8(
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
 DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS mutual CASCADE;
+DROP VIEW IF EXISTS recNum CASCADE; 
 
 -- Define views for your intermediate steps here:
+CREATE VIEW mutual AS
+SELECT Request.request_id, driver_id, client_id, DriverRating.rating as DRate, ClientRating.rating as CRate
+FROM DriverRating, ClientRating, Request, Dispatch, ClockedIn
+WHERE DriverRating.request_id = ClientRating.request_id AND ClientRating.request_id = Request.request_id AND Request.request_id = Dispatch.request_id AND ClockedIn.shift_id = Dispatch.shift_id;
 
+CREATE VIEW recNum AS
+SELECT client_id, count(client_id) as reciprocals, avg(DRate - CRate) as difference
+FROM mutual
+GROUP BY client_id;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q8
+SELECT *
+FROM recNum;
