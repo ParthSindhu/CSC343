a3dca300c893ac649f8a7e0543872a144e741514
TunaFish-1 <81477203+TunaFish-1@users.noreply.github.com>
2022-10-29 16:37:27 -0400

    Update a2.py

--- A2/part2/a2.py
+++ A2/part2/a2.py
@@ -255,7 +255,7 @@ class Assignment2:
             """)
             cursor.execute("""
                     CREATE TEMPORARY VIEW shiftOngoing AS
-                    (SELECT shift_id FROM ClockedIn) MINUS (SELECT shift_id FROM shiftOver)
+                    (SELECT shift_id FROM ClockedIn) - (SELECT shift_id FROM shiftOver)
             """)
             cursor.execute("""
                     SELECT shift_id, driver_id
